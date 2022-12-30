0a9d6d5f81c508e3b174deeadc2a2c1a8d1ecc72
Mattthew Pourroy <69867050+pourroymatt750@users.noreply.github.com>
2022-12-29 21:12:29 -0700

    Add files via upload

---
+++ c195 (1)/src/main/java/pourroy/c195/DAO/CustomerDao.java
@@ -0,0 +1,141 @@
+package pourroy.c195.DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import pourroy.c195.model.*;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CustomerDao {
+
+    private static int customer_id;
+
+    private static int division_id;
+
+    /**
+     * Gets list of all customers
+     * @throws SQLException from Prepared Statement
+     */
+    public static ObservableList<Customers> getAllCustomers() throws SQLException {
+        ObservableList<Customers> customersList = FXCollections.observableArrayList();
+
+        try {
+            //SQL statement
+            String sql = "SELECT Customer_Name, Address, Division, Country, Postal_Code, Phone " +
+                         "FROM customers, first_level_divisions, countries " +
+                         "WHERE customers.Division_ID=first_level_divisions.Division_ID AND " +
+                         "first_level_divisions.Country_ID=countries.Country_ID";
+
+
+            PreparedStatement ps = DBConnection.connection.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                //get results data
+                String Customer_Name = rs.getString("Customer_Name");
+                String Address = rs.getString("Address");
+                String Division = rs.getString("Division");
+                String Country = rs.getString("Country");
+                String Postal_Code = rs.getString("Postal_Code");
+                String Phone = rs.getString("Phone");
+
+                Customers c = new Customers(Customer_Name, Address, Division, Country, Postal_Code, Phone);
+                customersList.add(c);
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+
+        return customersList;
+    }
+
+    //Add new customer helper function
+    public static void createCustomer(String customer_name, String address, String postal_code,
+                                      String phone, FirstLevelDivisions division, Countries country) {
+
+        int divisionID = division.getDivision_id();
+        System.out.println(divisionID);
+
+        try {
+            //SQL Statement Customers Table
+            String sql2 = "INSERT INTO customers VALUES(null, ?, ?, ?, ?, null, null, null, null, ?)";
+
+            PreparedStatement ps2 = DBConnection.connection.prepareStatement(sql2);
+
+            ps2.setString(1, String.valueOf(customer_name));
+            ps2.setString(2, String.valueOf(address));
+            ps2.setString(3, String.valueOf(postal_code));
+            ps2.setString(4, String.valueOf(phone));
+            ps2.setInt(5, divisionID);
+
+            ps2.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Modify customer helper function
+    public static void modifyCustomer(int country_id, int division_id, int customer_id, String customer_name, String address,
+                                      String postal_code, String phone, FirstLevelDivisions division, Countries country) {
+
+        try {
+            //SQL Statement for Customers Table
+            String sql = "UPDATE customers set Customer_Name = ?, Address = ?, Postal_Code = ?, Phone = ?, Division_ID = ? WHERE Customer_ID = ?";
+
+            PreparedStatement pst = DBConnection.connection.prepareStatement(sql);
+            pst.setString(1, customer_name);
+            pst.setString(2, address);
+            pst.setString(3, postal_code);
+            pst.setString(4, phone);
+            pst.setInt(5, division_id);
+            pst.setInt(6, customer_id);
+
+            pst.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteCustomer(int Customer_ID) {
+        try {
+            //SQL Statement
+            String sql = "DELETE FROM customers WHERE Customer_ID = ?";
+
+            PreparedStatement pst = DBConnection.connection.prepareStatement(sql);
+
+            pst.setInt(1, Customer_ID);
+
+            pst.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getCustomerId(String customer) throws SQLException{
+        int customer_id = 0;
+
+        try {
+            String sql = "SELECT * FROM customers WHERE Customer_Name=?";
+
+            PreparedStatement pst = DBConnection.connection.prepareStatement(sql);
+
+            pst.setString(1, String.valueOf(customer));
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) { customer_id = rs.getInt("Customer_ID"); }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customer_id;
+    }
+
+}
---
+++ c195 (1)/src/main/java/pourroy/c195/MainController.java
@@ -0,0 +1,350 @@
+package pourroy.c195;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import pourroy.c195.DAO.AppointmentDoa;
+import pourroy.c195.DAO.CustomerDao;
+import pourroy.c195.DAO.DBConnection;
+import pourroy.c195.model.Appointments;
+import pourroy.c195.model.Customers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+/**
+ * Controller for the Main Page that provides functionality for the application
+ *
+ * @author matthewpourroy
+ */
+public class MainController {
+    /**
+     * BEGIN Customer
+     * Tableview Customers Table
+     */
+    @FXML
+    private TableView<Customers> customersTable;
+
+    /**
+     * VARCHAR(50) Customer Name
+     */
+    @FXML
+    private TableColumn customer_name;
+
+    /**
+     * VARCHAR(100) Address
+     */
+    @FXML
+    private TableColumn address;
+
+    /**
+     * VARCHAR(50) Division
+     * Originates from First-Level Divisions
+     */
+    @FXML
+    private TableColumn division;
+
+    /**
+     * VARCHAR(50) Country
+     * Originates from Countries Model
+     */
+    @FXML
+    private TableColumn country;
+
+    /**
+     * VARCHAR(50) Postal Code
+     */
+    @FXML
+    private TableColumn postal_code;
+
+    /**
+     * VARCHAR(50) Phone
+     */
+    @FXML
+    private TableColumn phone;
+
+    /**
+     * Takes user to Add Customer Page
+     *
+     * @param actionEvent Add Customer Button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onAddCustomerBtn(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("add-customer.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("Add Customer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static Customers selectedCustomer;
+
+    public static void setSelectedCustomer(Customers selected) {
+        selectedCustomer = selected;
+    }
+
+    public static Customers getSelectedCustomer() { return selectedCustomer; }
+
+    /**
+     * Takes user to Modify Customer Page
+     *
+     * @param actionEvent Modify Customer Button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onModifyCustomerBtn(ActionEvent actionEvent) throws IOException, SQLException {
+        Customers customer = customersTable.getSelectionModel().getSelectedItem();
+        MainController.setSelectedCustomer(selectedCustomer);
+
+        if (customer == null) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING");
+            alert.setContentText("No customer selected, please try again");
+            Optional<ButtonType> result = alert.showAndWait();
+        } else {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("modify-customer.fxml"));
+                loader.load();
+
+                ModifyCustomerController modCustController = loader.getController();
+                modCustController.sendCustomer(customersTable.getSelectionModel().getSelectedItem());
+
+                Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+                Parent root = loader.getRoot();
+                Scene scene = new Scene(root, 800, 600);
+                stage.setTitle("Modify Customer");
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * Checks to see if customer has any appointments:
+     * If customer has an appointment, then an error is thrown saying
+     * "Cannot delete a customer who has an appointment scheduled. Please delete customer appointments
+     * before deleting the customer".
+     * If customer does NOT have an appointment, then dialog box populates asking to confirm if user
+     * wants to delete the customer
+     *
+     * Deletes customer out of database
+     *
+     *
+     * @param actionEvent Delete Customer Button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onDeleteCustomerBtn(ActionEvent actionEvent) throws IOException, SQLException {
+        Customers customer = customersTable.getSelectionModel().getSelectedItem();
+        int customerId = CustomerDao.getCustomerId(customer.getCustomer_name());
+
+        CustomerDao.deleteCustomer(customerId);
+        updateCustomerTable();
+    }
+    /**
+     * END Customers
+     */
+
+    /**
+     * BEGIN Appointments
+     * Tableview Appointments Table
+     */
+    @FXML
+    private TableView<Appointments> appointmentsTable;
+
+    /**
+     * INT(10) Appointment ID
+     */
+    @FXML
+    private TableColumn appointment_id;
+
+    /**
+     * VARCHAR(50) Title
+     */
+    @FXML
+    private TableColumn title;
+
+    /**
+     * VARCHAR(50) Description
+     */
+    @FXML
+    private TableColumn description;
+
+    /**
+     * VARCHAR(50) Location
+     */
+    @FXML
+    private TableColumn appointment_location;
+
+    /**
+     * VARCHAR(50) Spelled as "Contact" in TableView
+     * Originates from Contacts Model
+     */
+    @FXML
+    private TableColumn contact_name;
+
+    /**
+     * VARCHAR(50) Type
+     */
+    @FXML
+    private TableColumn type;
+
+    /**
+     * DATETIME Start
+     */
+    @FXML
+    private TableColumn start;
+
+    /**
+     * DATETIME Start
+     */
+    @FXML
+    private TableColumn end;
+
+    /**
+     * INT(10) Customer ID
+     * Foreign Key from Customers Model
+     */
+    @FXML
+    private TableColumn customer_id;
+
+    /**
+     * INT(10)
+     * Foreign Key from Users Model
+     */
+    @FXML
+    private TableColumn user_id;
+
+    /**
+     * Takes user to Add Appointment Page
+     *
+     * @param actionEvent Add Appointment Button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onAddApptBtn(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("add-appointment.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("Add Appointment");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static Appointments selectedAppointment;
+
+    public static void setSelectedAppointment(Appointments selected) {
+        selectedAppointment = selected;
+    }
+
+    public static Appointments getSelectedAppointment() { return selectedAppointment; }
+
+    /**
+     * Takes user to Modify Appointment Page
+     *
+     * @param actionEvent Modify Appointment Button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onModifyApptBtn(ActionEvent actionEvent) throws IOException, SQLException {
+        Appointments appointment = appointmentsTable.getSelectionModel().getSelectedItem();
+        MainController.setSelectedAppointment(selectedAppointment);
+
+        if (appointment == null) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("WARNING");
+            alert.setContentText("No appointment selected, please try again");
+            Optional<ButtonType> result = alert.showAndWait();
+        } else {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("modify-appointment.fxml"));
+                loader.load();
+
+                ModifyAppointmentController modApptController = loader.getController();
+                modApptController.sendAppointment(appointmentsTable.getSelectionModel().getSelectedItem());
+
+                Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+                Parent root = loader.getRoot();
+                Scene scene = new Scene(root, 800, 600);
+                stage.setTitle("Modify Appointment");
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * Deletes Appointment
+     *
+     * @param actionEvent Delete Button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onDeleteApptBtn(ActionEvent actionEvent) throws IOException, SQLException {
+
+        Appointments appointment = appointmentsTable.getSelectionModel().getSelectedItem();
+        int appointmentId = appointment.getAppointment_id();
+
+        AppointmentDoa.deleteAppointment(appointmentId);
+        updateAppointmentTable();
+    }
+    /**
+     * END Appointments
+     */
+
+    /**
+     * Exits the program
+     *
+     * @param actionEvent Exit Button
+     */
+    public void onExitBtn(ActionEvent actionEvent) { System.exit(0); }
+
+    public void updateCustomerTable() throws SQLException{
+        customersTable.setItems(CustomerDao.getAllCustomers());
+        customer_name.setCellValueFactory(new PropertyValueFactory<>("customer_name"));
+        address.setCellValueFactory(new PropertyValueFactory<>("address"));
+        division.setCellValueFactory(new PropertyValueFactory<>("division"));
+        country.setCellValueFactory(new PropertyValueFactory<>("country"));
+        postal_code.setCellValueFactory(new PropertyValueFactory<>("postal_code"));
+        phone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+    }
+
+    public void updateAppointmentTable() throws SQLException{
+        appointmentsTable.setItems(AppointmentDoa.getAllAppointments());
+        appointment_id.setCellValueFactory(new PropertyValueFactory<>("appointment_id"));
+        title.setCellValueFactory(new PropertyValueFactory<>("title"));
+        description.setCellValueFactory(new PropertyValueFactory<>("description"));
+        appointment_location.setCellValueFactory(new PropertyValueFactory<>("appointment_location"));
+        contact_name.setCellValueFactory(new PropertyValueFactory<>("contact_name"));
+        type.setCellValueFactory(new PropertyValueFactory<>("type"));
+        start.setCellValueFactory(new PropertyValueFactory<>("start"));
+        end.setCellValueFactory(new PropertyValueFactory<>("end"));
+        customer_id.setCellValueFactory(new PropertyValueFactory<>("customer_id"));
+        user_id.setCellValueFactory(new PropertyValueFactory<>("user_id"));
+    }
+
+    public void initialize() throws SQLException {
+        updateCustomerTable();
+        updateAppointmentTable();
+    }
+}
