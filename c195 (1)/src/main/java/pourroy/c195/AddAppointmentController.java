69433a93cb2c3275d9072a61cb6ff47d9328ef17
Mattthew Pourroy <69867050+pourroymatt750@users.noreply.github.com>
2023-01-04 21:32:00 -0700

    Add files via upload

---
+++ c195 (1)/src/main/java/pourroy/c195/AddAppointmentController.java
@@ -0,0 +1,191 @@
+package pourroy.c195;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import pourroy.c195.DAO.*;
+import pourroy.c195.model.Appointments;
+import pourroy.c195.model.Contacts;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.*;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Controller for the Add Appointment Page that provides functionality for the application
+ *
+ * @author matthewpourroy
+ */
+public class AddAppointmentController {
+    /**
+     * INT(10) Appointment ID
+     * Primary Key
+     */
+    @FXML
+    private TextField appointment_id;
+
+    /**
+     * VARCHAR(50) Title
+     */
+    @FXML
+    private TextField title;
+
+    /**
+     * VARCHAR(50) Description
+     */
+    @FXML
+    private TextField description;
+
+    /**
+     * VARCHAR(50) Appointment Location
+     */
+    @FXML
+    private TextField appointment_location;
+
+    /**
+     * VARCHAR(50) Contact Name
+     */
+    @FXML
+    private ComboBox<Contacts> contact_name;
+
+    /**
+     * VARCHAR(50) Type
+     */
+    @FXML
+    private TextField type;
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private ComboBox<LocalTime> startTime;
+
+    @FXML
+    private ComboBox<LocalTime> endTime;
+
+    /**
+     * INT(10) Customer ID
+     */
+    @FXML
+    private TextField customer_id;
+
+    /**
+     * INT(10) User ID
+     */
+    @FXML
+    private TextField user_id;
+
+    /**
+     * Adds new appointment to the database and displays on the Main Page
+     *
+     * @param actionEvent Add button action
+     * @throws IOException from FXMLLoader
+     */
+    public void onAddBtn(ActionEvent actionEvent) throws IOException {
+        String apptTitle = title.getText();
+        String apptDescript = description.getText();
+        String apptLocation = appointment_location.getText();
+        Contacts apptContact = contact_name.getValue();
+        String apptType = type.getText();
+        LocalDateTime apptStart = LocalDateTime.of(startDate.getValue(), startTime.getValue());
+        LocalDateTime apptEnd = LocalDateTime.of(endDate.getValue(), endTime.getValue());
+        int apptCustId = Integer.parseInt(customer_id.getText());
+        int apptUserId = Integer.parseInt(user_id.getText());
+
+        LocalDate apptStartDate = startDate.getValue();
+        LocalTime apptStartTime = startTime.getValue();
+        LocalDate appEndDate = endDate.getValue();
+        LocalTime appEndTime = endTime.getValue();
+
+        LocalTime startBizHrs = LocalTime.of(8, 0);
+        LocalTime endBizHrs = LocalTime.of(22, 0);
+        ZoneId estZoneId = ZoneId.of("America/New_York");
+        ZoneId userZoneId = ZoneId.of(String.valueOf(ZoneId.systemDefault()));
+
+        ZonedDateTime startBizHrsEst = ZonedDateTime.of(apptStartDate, startBizHrs, estZoneId);
+        ZonedDateTime endBizHrsEst = ZonedDateTime.of(appEndDate, endBizHrs, estZoneId);
+
+        ZonedDateTime startAppt = ZonedDateTime.of(apptStartDate, apptStartTime, userZoneId);
+        ZonedDateTime endAppt = ZonedDateTime.of(appEndDate, appEndTime, userZoneId);
+
+
+        if (apptTitle == null || apptDescript == null || apptLocation == null || apptContact == null
+                || apptType == null || apptStart == null || apptEnd == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setContentText("All fields must be filled in, no field can be left blank. Please try again");
+            Optional<ButtonType> result = alert.showAndWait();
+        } else if (startAppt.isBefore(startBizHrsEst) || endAppt.isAfter(endBizHrsEst)) {
+            Alert alert2 = new Alert(Alert.AlertType.WARNING);
+            alert2.setTitle("WARNING");
+            alert2.setContentText("Appointment must be scheduled during business hours, which are 8am-10pm daily");
+            Optional<ButtonType> result = alert2.showAndWait();
+        } else {
+        //Creates new appointment
+           AppointmentDoa.createAppointment(apptTitle, apptDescript, apptLocation, apptContact,
+           apptType, apptStart, apptEnd, apptCustId, apptUserId);
+
+        //Sends user back to Home Page
+            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+            Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1350, 750);
+            stage.setTitle("Home");
+            stage.setScene(scene);
+            stage.show();
+    }
+}
+
+    /**
+     * Shows Confirmation dialog and if user hits "OK" then it cancels any data user entered and returns to
+     * main page
+     *
+     * @param actionEvent Cancel Button
+     * @throws IOException from FXMLLoader
+     */
+    public void onCancelBtn(ActionEvent actionEvent) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("CONFIRMATION");
+        alert.setContentText("Are you sure you want to cancel? All data currently entered will be lost");
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+            Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 1350, 750);
+            stage.setTitle("Home");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    public void initialize() throws SQLException {
+       LocalTime start = LocalTime.of(5, 0);
+       LocalTime end = LocalTime.of(23, 0);
+
+       while (start.isBefore(end.plusSeconds(1))) {
+           startTime.getItems().add(start);
+           endTime.getItems().add(start);
+           start = start.plusMinutes(10);
+       }
+       startTime.getSelectionModel().select(LocalTime.of(8,0));
+       endTime.getSelectionModel().select(LocalTime.of(22,0));
+
+       contact_name.setItems(ContactDoa.getAllContacts());
+
+    }
+}
---
+++ c195 (1)/src/main/java/pourroy/c195/DAO/AppointmentDoa.java
@@ -0,0 +1,166 @@
+package pourroy.c195.DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import pourroy.c195.model.Appointments;
+import pourroy.c195.model.Contacts;
+import pourroy.c195.model.Customers;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class AppointmentDoa {
+
+    public static int contact_id;
+
+    public static ObservableList<Appointments> getAllAppointments() throws SQLException {
+        ObservableList<Appointments> appointmentsList = FXCollections.observableArrayList();
+
+        try {
+            //SQL statement
+            String sql = "SELECT Appointment_ID, Title, Description, Location, contacts.Contact_Name, Type, Start, End, Customer_ID, User_ID "
+                       + "FROM appointments, contacts "
+                       + "WHERE appointments.Contact_ID=contacts.Contact_ID";
+
+
+            PreparedStatement ps = DBConnection.connection.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                //get results data
+                int Appointment_ID = rs.getInt("Appointment_ID");
+                String Title = rs.getString("Title");
+                String Description = rs.getString("Description");
+                String Location = rs.getString("Location");
+                String Contact_Name = rs.getString("Contact_Name");
+                String Type = rs.getString("Type");
+                LocalDateTime Start = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDateTime End = rs.getTimestamp("End").toLocalDateTime();
+                int Customer_ID = rs.getInt("Customer_ID");
+                int User_ID = rs.getInt("User_ID");
+
+                Appointments a = new Appointments(Appointment_ID, Title, Description, Location, Contact_Name, Type, Start, End, Customer_ID, User_ID);
+                appointmentsList.add(a);
+            }
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return appointmentsList;
+    }
+
+    public static void createAppointment(String title, String description, String appointment_location,
+                                         Contacts contact_name, String type,LocalDateTime start,
+                                         LocalDateTime end, int customer_id, int user_id) {
+
+        try {
+            //SQL Statement in Appointments Table
+            String sql1 = "INSERT INTO appointments VALUES(null, ?, ?, ?, ?, ?, ?, null, null, null, null, ?, ?, ?)";
+
+            PreparedStatement ps1 = DBConnection.connection.prepareStatement(sql1);
+
+            ps1.setString(1, String.valueOf(title));
+            ps1.setString(2, String.valueOf(description));
+            ps1.setString(3, String.valueOf(appointment_location));
+            ps1.setString(4, String.valueOf(type));
+            ps1.setTimestamp(5, Timestamp.valueOf(start));
+            ps1.setTimestamp(6, Timestamp.valueOf(end));
+            ps1.setInt(7, customer_id);
+            ps1.setInt(8, user_id);
+            ps1.setInt(9, contact_name.getContact_id());
+
+            ps1.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void modifyAppointment(int appointment_id, String title, String description,
+                                         String appointment_location, Contacts contact_name, String type, LocalDateTime start,
+                                         LocalDateTime end, int customer_id, int user_id, int contact_id) {
+        try {
+            //SQL Statement
+            String sql = "UPDATE appointments SET Title = ?, Description = ?, Location = ?, Type = ?, " +
+                    "Start = ?, End = ?, Customer_ID = ?, User_ID = ?, Contact_ID = ? WHERE Appointment_ID = ?";
+
+            PreparedStatement pst = DBConnection.connection.prepareStatement(sql);
+            pst.setString(1, title);
+            pst.setString(2, description);
+            pst.setString(3, appointment_location);
+            pst.setString(4, type);
+            pst.setTimestamp(5, Timestamp.valueOf(start));
+            pst.setTimestamp(6, Timestamp.valueOf(end));
+            pst.setInt(7, customer_id);
+            pst.setInt(8, user_id);
+            pst.setInt(9, contact_id);
+            pst.setInt(10, appointment_id);
+
+            pst.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteAppointment(int Appointment_ID) {
+        try {
+            //SQL Statement
+            String sql = "DELETE FROM appointments WHERE Appointment_ID = ?";
+
+            PreparedStatement pst = DBConnection.connection.prepareStatement(sql);
+
+            pst.setInt(1, Appointment_ID);
+
+            pst.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ObservableList<Appointments> getAppointmentByMonth(LocalDateTime start) throws SQLException {
+        ObservableList<Appointments> appointmentsList = FXCollections.observableArrayList();
+
+        System.out.println(start);
+
+        try {
+            //SQL Statement
+            String sql = "SELECT Appointment_ID, Title, Description, Location, contacts.Contact_Name, " +
+                    "Type, Start, End, Customer_ID, User_ID" +
+                    " FROM appointments, contacts WHERE start < Start AND appointments.Contact_ID=contacts.Contact_ID";
+
+            PreparedStatement pst = DBConnection.connection.prepareStatement(sql);
+
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                //get results data
+                int Appointment_ID = rs.getInt("Appointment_ID");
+                String Title = rs.getString("Title");
+                String Description = rs.getString("Description");
+                String Location = rs.getString("Location");
+                String Contact_Name = rs.getString("Contact_Name");
+                String Type = rs.getString("Type");
+                LocalDateTime Start = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDateTime End = rs.getTimestamp("End").toLocalDateTime();
+                int Customer_ID = rs.getInt("Customer_ID");
+                int User_ID = rs.getInt("User_ID");
+
+                Appointments a = new Appointments(Appointment_ID, Title, Description, Location, Contact_Name, Type, Start, End, Customer_ID, User_ID);
+                appointmentsList.add(a);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(appointmentsList);
+        return appointmentsList;
+    }
+
+}
